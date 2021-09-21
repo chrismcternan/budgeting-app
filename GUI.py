c450c4e5624420d219860d6ebd6cc40ba6296e31
chrismcternan <cmcternan24@gmail.com>
2021-09-20 22:23:50 -0400

    Changes to class based windows and creates select ledger feature

--- GUI.py
+++ GUI.py
@@ -4,49 +4,100 @@ import os
 
 from ledger import *
 
-# Creates window for adding a new ledger
-def open_create_ledger():
-    create_ledger_window = tk.Toplevel()
-    create_ledger_window.geometry("400x300")
-    label1 = tk.Label(create_ledger_window, text="Create a new ledger").pack()
-    filename = tk.StringVar()
-    def submit():
-        new_filename = filename.get()
-        create_ledger_window.destroy()
-        create_ledger(new_filename)
-    # Initialize Tkinter window
-    new_ledger_entry = tk.Entry(create_ledger_window, textvariable=filename).pack()
-    submit_bt = tk.Button(create_ledger_window, text="Submit", command=submit).pack()
 
-# Initilize root window for mainloop
-def init_root():
-    # Initialize Tkinter window
-    root = tk.Tk()
-    root.geometry("400x300")
-    # Header display
-    root.title("Budgeting Manager")
-    header = tk.Label(root, text="Select a ledger").pack()
-    # Upload CSV button
-    # upload_button = tk.Button(
-    #     window,
-    #     text = "Upload CSV",
-    #     command = upload_csv()
-    # ).pack()
-    create_ledger_bt = tk.Button(
-        root,
-        text = "Create Ledger",
-        command = open_create_ledger
-    ).pack()
-    # Download ledgers button
-    dl_ledgers_bt = tk.Button (
-        root,
-        text = "Download Ledgers",
-        command = init_ledgers
-    ).pack()
-    root.mainloop()
+# initialize GUI Errors
+class NoLedgersError(Error):
+    # raises when there are no ledgers, and one must be created
+    pass
+
+
+# initialize ledgers and open ledger selection window
+ledgers = init_ledgers()
+active_ledger = None      # default active_ledger
+
+
+# initializes class for tk window for creating new ledger
+class CreateLedgerWindow():
+    def __init__(self):
+        window = tk.Toplevel()
+        window.geometry("400x300")
+        label = tk.Label(window, text = "Enter a name for new ledger").pack()
+
+        # create submit function for new ledger
+        filename = tk.StringVar()
+        def submit(filename):
+            new_filename = filename.get()
+            window.destroy()
+            # sets new active ledger and calls create function
+            active_ledger = create_ledger(new_filename, ledgers)
+            print("'" + active_ledger.name + "' selected as active ledger")
+        new_ledger_entry = tk.Entry(window, textvariable=filename).pack()
+        submit_bt = tk.Button(
+            window,
+            text = "Submit",
+            command= lambda: submit(filename)
+        ).pack()
+
+
+# open window to select the active ledger
+class SelectLedgerWindow():
+    def __init__(self):
+        window = tk.Toplevel()
+        window.geometry("400x300")
+
+        # define function to select active_ledger
+        filename = tk.StringVar(window)
+        def submit(filename):
+            window.destroy()
+            # active_ledger = ledgers(name=filename)
+            # print (filename.get())
+            for ledger in ledgers:
+                if filename.get() == ledger.name:
+                    active_ledger = ledger
+                    break
+            print("'" + active_ledger.name + "' selected as active ledger")
+
+        # set up window objects 
+        try:
+            if len(ledgers) < 1:
+                raise NoLedgersError
+            else:
+                om = tk.OptionMenu(window, filename, *ledgers).pack()
+                submit_bt = tk.Button(
+                    window,
+                    text = "Submit",
+                    command= lambda: submit(filename)
+                ).pack()
+        except NoLedgersError:
+            # if no ledger found, goes straight to create new ledger
+            window.destroy()
+            CreateLedgerWindow()
+            print ("No ledgers, please create a new ledger")
+
+
+class ApplicationWindow():
+    def __init__(self, master=None):
+        root = tk.Tk()              # initialize root window
+        root.geometry("400x300")    # sets window size
+        root.title("Budgeting Manager")
+        # Upload CSV to ledger button
+        upload_button = tk.Button(
+            root,
+            text = "Upload CSV",
+            command = lambda: active_ledger.upload_csv
+        ).pack()
+        # Change Ledger button
+        ledger_button = tk.Button(
+            root,
+            text = "Select Ledger",
+            command = SelectLedgerWindow
+        ).pack()
+        # run mainloop
+        root.mainloop()
+
 
 def main():
-    init_root()
+    app = ApplicationWindow()
 
 if __name__ == "__main__":
     main()
--- ledger.py
+++ ledger.py
@@ -2,21 +2,13 @@ from tkinter import filedialog
 import pandas as pd
 import os
 
-# only run this code when importing modules
-# def main():
-#     pass
-
-# if __name__ == "__main__":
-#     main()
 
 # Set up ledgers directory
 dir_name = "ledgers"
 parent_dir = os.getcwd()
 ledger_dir = os.path.join(parent_dir, dir_name)
 if not os.path.isdir(ledger_dir):
-    os.mkdir(ledger_dir)
-
-# create folder if non exists
+    os.mkdir(ledger_dir) # create folder if non exists
 
 
 # Define error classes
@@ -32,31 +24,62 @@ class InvalidCharacterError(Error):
 
 # define methods for manipulating and loading ledger data
 class TransactionDatabase():
-    
-    def download():
+    cleaned_header = [
+        "Account Name",
+        "Account Type",
+        "Credit Card No",
+        "Transaction Date",
+        "Posted Date",
+        "Description",
+        "Category",
+        "Sub-category",
+        "Amount",
+        "MCC",
+        "Cashback Reward",
+        "Notes"
+    ]
+    ledger_df = pd.DataFrame(columns=cleaned_header)
+
+    def download(self, *args, **kwargs):
         # download data to dataframe
-        print("Code Here")
+        print(self.ledger_df.head(5))
 
-    def save():
-        # 
-        print("code here")
+    def save(self, *args, **kwargs):
+        # save dataframe to a csv file under ledger.name
+        path = os.path.join(ledger_dir, self.name)
+        self.ledger_df.to_csv(path)
 
+    def upload_csv(self, *args, **kwargs):
+        file_name = filedialog.askopenfilename()
+        print("Uploading: " + file_name)
 
+        with open(file_name) as f:
+            upload_df = pd.read_csv(f, header=0)
+        """clean data to match headers so it can be merged with csv
+        with different formatting"""
+        # clean amount charged
+        print (upload_df.head(5))
+        if "Amount" not in upload_df.columns:
+            # if no amount column, create new column
+            # for statements that use debit/credit columns
+            upload_df['Credit'] = upload_df.fillna(0)['Credit']
+            upload_df['Debit'] = upload_df.fillna(0)['Debit']
+            new_amount = upload_df["Credit"] - upload_df["Debit"]
+            upload_df["Amount"] = new_amount
+            print (upload_df.head(5))
+        # Merge cleaned data with ledger_df
+        for col in upload_df.columns:
+            if col in self.cleaned_header:
+                self.ledger_df[col] = upload_df[col]
+        self.save()
 
     def __init__(self, name):
         self.name = name
-        print("Ledger '" + name + "' created.")
+        print("Ledger '" + name + "' initialized.")
 
     def __str__(self):
         return self.name    # set name of objects
 
-
-def upload_csv():
-    file_name = filedialog.askopenfilename()
-    print("Uploading: " + file_name)
-
-    with open(file_name) as f:
-        upload_df = pd.read_csv(f, header=0)
 
 def init_ledgers():
     ledgers = []
@@ -64,7 +87,7 @@ def init_ledgers():
         ledgers.append(TransactionDatabase(name = file))
     return ledgers
 
-def create_ledger(filename):
+def create_ledger(filename, ledgers):
     """create new object of class Transaction Database with 
     filename defined from tkinter window"""
     
@@ -82,6 +105,10 @@ def create_ledger(filename):
         filepath = os.path.join(ledger_dir, filename)
         with open(filepath, 'w+') as fp:
             pass
+        # appends new ledger object to ledgers and returns
+        new_ledger = TransactionDatabase(name = filename)
+        ledgers.append(new_ledger)
+        return new_ledger
 
 def select_ledger():
         file_name = filedialog.askopenfilename()
@@ -90,6 +117,15 @@ def select_ledger():
         with open(file_name) as f:
             df = pd.read_csv(f, header=0)
 
-# ledgers = create_ledger()
-# for ledger in ledgers:
-#     print (ledger)
+def main():
+    pass
+    # ledgers = init_ledgers()
+    # for ledger in ledgers:
+    #     ledger.upload_csv()
+    #     ledger.save()
+    #     # print (ledger)
+    #     # ledger.download()
+    #     break
+
+if __name__ == "__main__":
+    main()
